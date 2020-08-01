61ef4b0c0b996f72af67f3e93c19b00c918c8200
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-08-01 18:48:26 +0530

    Deskew Operation Included

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 from Text_Localization.text_localization import localTextRegion
 from Table_Start_Detection.detect_table import detectTable
 from Table_Start_Detection.crop_table import cropTable
+from Deskew.deskewImage import deskewImage
 import cv2
 
-fileName = 'im7_full.png'
+fileName = 'im22_full.png'
+
+# Deskewing Image
+print("Startied Process....")
+print("Deskewing Image....")
+image = cv2.imread('../images/'+fileName)
+deskewImage(image)
+print("Deskewing Completed!!!")
+
 
 # Detecting and Separating MetaData and Table out of the invoice Image
-print("Startied Process....")
 print("Table and Meta Data Separation Started...")
 tablePosition = detectTable(fileName)
-image = cv2.imread('../images/'+fileName)
+image = cv2.imread('../results/deskew_image/image.png')
 cropTable(image, tablePosition)
 print("Table and Meta Data Separation Completed!!!\n\n")
 
